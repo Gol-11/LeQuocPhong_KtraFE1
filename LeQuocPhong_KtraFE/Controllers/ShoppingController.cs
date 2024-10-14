6b7efa2cc3f5c898f96abdb669e3cec22410ffe7
HNC <HNC@DESKTOP-392F22O>
2024-10-14 11:57:02 +0700

    Add project files.

---
+++ LeQuocPhong_KtraFE/Controllers/ShoppingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeQuocPhong_KtraFE.Controllers
+{
+    public class ShoppingController : Controller
+    {
+      
+        public ActionResult ShoppingCart()
+        {
+            return View();
+        }
+    }
+}
